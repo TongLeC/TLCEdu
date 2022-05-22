e0dd8e8a8601fce0470a1b354a630dd4536dd12b
Michael <michael.li@tonglec.org>
2022-05-21 22:30:54 -0700

    Temp changes to config and package.json

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/TLCEdu/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service');
+
+let toExport = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config =>{
+    config.plugin('html').tap(args => {
+        args[0].title = '同乐教育';
+        return args;
+    })
+  }
+})
+
+toExport.publicPath = process.env.NODE_ENV === 'production'
+  ? '/TLCEdu/'
+  : '/';
+
+module.exports = toExport;
