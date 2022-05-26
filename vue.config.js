fa812cc7271008d7e6b37cafb094befa08338fba
Michael <michael.li@tonglec.org>
2022-05-25 17:53:33 -0700

    2. Fix 404 for route catching

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  chainWebpack: config =>{
+  chainWebpack: config => {
     config.plugin('html').tap(args => {
         args[0].title = '同乐教育';
         return args;
