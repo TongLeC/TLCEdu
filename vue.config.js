28660b73c86736c1c8a7a35919caf3b40c5aaebc
Michael11233 <86174052+Michael11233@users.noreply.github.com>
2022-05-26 19:01:02 -0700

    Feature/2 deployment (#10)
    * 2. move app contents to top level
    
    * 2. add publicpath and 404 fallback page
    
    * adding dist subtree
    
    * adding dist subtree
    
    * adding dist subtree
    
    * 2. Fix 404 for route catching
    
    * Delete dist directory

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  chainWebpack: config =>{
+  chainWebpack: config => {
     config.plugin('html').tap(args => {
         args[0].title = '同乐教育';
         return args;
     })
-}
+  },
+  publicPath: '/'
 })
