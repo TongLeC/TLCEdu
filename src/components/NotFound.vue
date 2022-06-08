45caab3ffca89fc0d15a42bb76eddbc3d30dfa90
jia-rong-lu <jiarong.lu@tonglec.org>
2022-06-08 10:03:16 +0800

    Bug/11.No wildcard route

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,23 +1,29 @@
 <template>
   <div>
-      Not Found
-      404
+    Not Found 404
+    <a href="/" class="return-button">返回首页</a>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NotFound',
-  data () {
-    return {
-    }
+  name: "NotFound",
+  data() {
+    return {};
   },
-  methods: {}
-}
+  methods: {},
+};
 </script>
 
 <style scoped lang='scss'>
 div {
-    font-size: 10rem;
+  font-size: 10rem;
+  .return-button {
+    display: block;
+    padding: 15px 20px;
+    background: rgb(230, 230, 230);
+    color: #ea4335;
+    font-size: 3rem;
+  }
 }
 </style>
